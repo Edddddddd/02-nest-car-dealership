2fd05bd275559552a02a1910b7da7ffc9a508870
Eduardo Silva <esilva@bice.cl>
2023-07-04 22:40:41 -0400

    crud complete

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,6 +1,11 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Car } from './interfaces/car.interface';
 import { v4 as uuid } from 'uuid';
+import { CreateCarDto, UpdateCarDto } from './dto';
 @Injectable()
 export class CarsService {
   private cars: Car[] = [
@@ -29,5 +34,34 @@ export class CarsService {
     const car = this.cars.find((cars) => cars.id === id);
     if (!car) throw new NotFoundException(`Car with id '${id}' not found`);
     return car;
+  }
+
+  create(createCarDto: CreateCarDto) {
+    const car: Car = {
+      id: uuid(),
+      ...createCarDto,
+    };
+    this.cars.push(car);
+    return car;
+  }
+
+  update(id: string, updateCarDto: UpdateCarDto) {
+    let carDB = this.findOneBydId(id);
+
+    if (updateCarDto.id && updateCarDto.id !== id)
+      throw new BadRequestException(`Car id is not valid inside body`);
+
+    this.cars = this.cars.map((car) => {
+      if (car.id === id) {
+        carDB = { ...carDB, ...updateCarDto, id };
+        return carDB;
+      }
+    });
+    return carDB;
+  }
+
+  delete(id: string) {
+    this.findOneBydId(id);
+    this.cars = this.cars.filter((car) => car.id !== id);
   }
 }

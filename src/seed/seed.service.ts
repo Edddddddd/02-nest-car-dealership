2f4d5420bc175473ff653b2515a365c8a482b319
Eduardo Silva <esilva@bice.cl>
2023-07-10 18:25:19 -0400

    add seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { CARS_SEDD } from './data/cars.data';
+import { BRANDS_SEDD } from './data/brads.data';
+import { CarsService } from '../cars/cars.service';
+import { BrandsService } from '../brands/brands.service';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandsService: BrandsService,
+  ) {}
   populateDB() {
+    this.carsService.fillCarsWithSeedData(CARS_SEDD);
+    this.brandsService.fillBrandsWithSeedData(BRANDS_SEDD);
     return 'Seed executed';
   }
 }
